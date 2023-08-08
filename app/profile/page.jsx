5797255346f25e1092fdb4a13bb497c9d46a690c
Dnzzk2 <83647184+Dnzzk2@users.noreply.github.com>
2023-08-08 11:52:23 +0800

    Update page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -17,6 +17,7 @@ const MyProfile = () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`)
       const data = await response.json()
 
+      
       setPosts(data)
     }
 
